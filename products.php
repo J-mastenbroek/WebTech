1ef300e763fb8b8e9d96b71d77af797f40090f3f
Mitch Boontjes <mitch.boontjes@gmail.com>
2023-01-17 16:32:25 +0100

    check of productnamen nu tevoorschijnkomen uit database

---
+++ products.php
@@ -0,0 +1,25 @@
+<?php
+    include_once('includes/connection.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+<?php
+    $sql = "SELECT * FROM products;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    if ($resultCheck > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['smaak'];
+
+        }
+    }
+
+
+?>
