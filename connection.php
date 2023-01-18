8e0bcf1a6b8384cf89a991e037d2a688fe9fef8f
Jesse <postvoorjesse@gmail.com>
2023-01-18 17:39:14 +0100

    database fixed

--- connection.php
+++ connection.php
@@ -2,15 +2,11 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
-
-
-
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbName = "webtech_database";
+$username = "mitchb";
+$password = "FSSbbuztalQlumSMqfEnYXRzTjXxMuNm";
+$dbName = "webtech";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbName);
@@ -19,7 +15,6 @@ $conn = mysqli_connect($servername, $username, $password, $dbName);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
 
 ?>
 
